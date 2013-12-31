3b961a4deb7f931d6028831d3ff5398219cd3abc
Manassarn Manoonchai <manassarn@gmail.com>
2013-12-31 22:52:43 +0700

    Add answer label

--- app/view_controllers/main_view_controller.rb
+++ app/view_controllers/main_view_controller.rb
@@ -4,7 +4,12 @@ class MainViewController < UIViewController
 
     @label = UILabel.alloc.initWithFrame CGRectMake(20, 20, 160, 40)
     @label.text = "Is it 2014 yet?"
-
     self.view.addSubview @label
+
+    @is_2014_label = UILabel.alloc.initWithFrame CGRectMake(0, 100, 320, 360)
+    @is_2014_label.text = "NO"
+    @is_2014_label.textAlignment = NSTextAlignmentCenter
+    @is_2014_label.font = UIFont.systemFontOfSize 168
+    self.view.addSubview @is_2014_label
   end
 end
--- spec/main_view_controller_spec.rb
+++ spec/main_view_controller_spec.rb
@@ -3,6 +3,7 @@ describe "MainViewController" do
 
   before do
     @label = controller.instance_variable_get "@label"
+    @is_2014_label = controller.instance_variable_get "@is_2014_label"
   end
 
   it "should exist" do
@@ -20,6 +21,20 @@ describe "MainViewController" do
 
     it "should have text" do
       @label.text.should == "Is it 2014 yet?"
+    end
+  end
+
+  describe "@is_2014_label" do
+    it "should exist" do
+      @is_2014_label.should.not == nil
+    end
+
+    it "should have text" do
+      @is_2014_label.text.should == "NO"
+    end
+
+    it "should align center" do
+      @is_2014_label.textAlignment.should == NSTextAlignmentCenter
     end
   end
 end
