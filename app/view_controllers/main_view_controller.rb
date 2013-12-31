bbde6b386e1314d7e51658a86b4396f89374d5ed
Manassarn Manoonchai <manassarn@gmail.com>
2013-12-31 22:33:57 +0700

    Set label frame

--- app/view_controllers/main_view_controller.rb
+++ app/view_controllers/main_view_controller.rb
@@ -2,7 +2,7 @@ class MainViewController < UIViewController
   def viewDidLoad
     super
 
-    @label = UILabel.alloc.init
+    @label = UILabel.alloc.initWithFrame CGRectMake(20, 20, 160, 40)
     @label.text = "Is it 2014 yet?"
 
     self.view.addSubview @label
