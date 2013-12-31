898f4a4153ad10a54ad81254a209db2abdd9996a
Manassarn Manoonchai <manassarn@gmail.com>
2013-12-31 21:20:27 +0700

    Add main view controller

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,9 +1,14 @@
 describe "Application 'isit2014yet'" do
   before do
     @app = UIApplication.sharedApplication
+    @window = @app.delegate.instance_variable_get "@window"
   end
 
   it "has one window" do
     @app.windows.size.should == 1
+  end
+
+  it "has rootViewController" do
+    @window.rootViewController.should.not == nil
   end
 end
---
+++ spec/main_view_controller_spec.rb
@@ -0,0 +1,7 @@
+describe "MainViewController" do
+  tests MainViewController
+
+  it "should exist" do
+    controller.should.not == nil
+  end
+end
