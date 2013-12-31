1fca801f8302deaf9d95f8188ae86d2c0d8912dd
Manassarn Manoonchai <manassarn@gmail.com>
2013-12-31 21:50:55 +0700

    Add label

--- app/view_controllers/main_view_controller.rb
+++ app/view_controllers/main_view_controller.rb
@@ -1,3 +1,10 @@
 class MainViewController < UIViewController
+  def viewDidLoad
+    super
 
+    @label = UILabel.alloc.init
+    @label.text = "Is it 2014 yet?"
+
+    self.view.addSubview @label
+  end
 end
--- spec/main_view_controller_spec.rb
+++ spec/main_view_controller_spec.rb
@@ -1,7 +1,25 @@
 describe "MainViewController" do
   tests MainViewController
 
+  before do
+    @label = controller.instance_variable_get "@label"
+  end
+
   it "should exist" do
     controller.should.not == nil
+  end
+
+  it "has @label as a subview" do
+    controller.view.subviews.should.include @label
+  end
+
+  describe "@label" do
+    it "should exist" do
+      @label.should.not == nil
+    end
+
+    it "should have text" do
+      @label.text.should == "Is it 2014 yet?"
+    end
   end
 end
